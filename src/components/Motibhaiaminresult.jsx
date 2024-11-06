c72497073cfd8943f873ed0c1a49bd24d3bc663f
ErbITservices <erbitservces@gmail.com>
2024-11-06 22:54:59 +0530

    mihir

--- src/components/Motibhaiaminresult.jsx
+++ src/components/Motibhaiaminresult.jsx
@@ -22,7 +22,7 @@ function Motibhaiaminresult() {
     console.log(category.current.value);
     try {
       const response = await axios.get(
-        `http://localhost:5555/${category.current.value}/`
+        `http://gramgranthalay-backend-2.onrender.com/${category.current.value}/`
       );
       console.log(response.data),
         response.data.sort(({ marks: a }, { marks: b }) => b - a);
@@ -37,7 +37,7 @@ function Motibhaiaminresult() {
     console.log(e);
     try {
           const response = await axios.get(
-            `http://localhost:5555/Motibhaiamin1/${e}`
+            `http://gramgranthalay-backend-2.onrender.com/${e}`
           );
           console.log(response.data), setlibrary(response.data);
         } catch (error) {
