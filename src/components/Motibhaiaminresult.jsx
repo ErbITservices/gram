947c502c8de5830579e091dae0f3040b27ff9fcd
ErbITservices <erbitservces@gmail.com>
2024-11-06 20:49:29 +0530

    mihir

--- src/components/Motibhaiaminresult.jsx
+++ src/components/Motibhaiaminresult.jsx
@@ -98,7 +98,7 @@ function Motibhaiaminresult() {
                       <td>{i.lname}</td>
                       <td>{i.gam}</td>
                       <td>{i.marks}</td>
-                      <td><button onClick={handleshowdata(e)}>Show data</button></td>
+                      <td><button onClick={handleshowdata}>Show data</button></td>
                     </tr>
                   ))}
                 </tbody>
