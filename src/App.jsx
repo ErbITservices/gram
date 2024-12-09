042cbd326d9d95adc90bc476c78760172eaf4ef7
MIHIRPATEL2715 <mihirpqtel@gmail.com>
2024-12-09 20:03:38 +0530

    mihir

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ import Motibhaiamin_4 from "./Forms/Motibhaiamin_4";
 import Motibhaiamin_5 from "./Forms/Motibhaiamin_5";
 import Motibhaiamin_6 from "./Forms/Motibhaiamin_6";
 import Motibhaiamin_7 from "./Forms/Motibhaiamin_7";
-// import Motibhaiamin1result from "./components/Motibhaiamin1result";
+import Motibhaiamin1result from "./components/Motibhaiamin1result";
 
 import Motibhaiamin2result from "./components/Motibhaiamin2result";
 import Motibhaiamin3result from "./components/Motibhaiamin3result";
@@ -134,10 +134,10 @@ function App() {
           <Route path="/Motibhaiamin_5" element={<Motibhaiamin_5 />}></Route>
           <Route path="/Motibhaiamin_6" element={<Motibhaiamin_6 />}></Route>
           <Route path="/Motibhaiamin_7" element={<Motibhaiamin_7 />}></Route>
-          {/* <Route
+          <Route
             path="/Motibhaiamin1result"
             element={<Motibhaiamin1result />}
-          ></Route> */}
+          ></Route>
           {/* <Route
             path="/motibhaiamin2result"
             element={<Motibhaiamin2result />}
