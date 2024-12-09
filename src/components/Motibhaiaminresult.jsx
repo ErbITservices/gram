896e82f5369c7dee074c2a2ae1869096b02e9af6
MIHIRPATEL2715 <mihirpqtel@gmail.com>
2024-12-09 20:14:08 +0530

    mihir

--- src/components/Motibhaiaminresult.jsx
+++ src/components/Motibhaiaminresult.jsx
@@ -35,11 +35,11 @@ function Motibhaiaminresult() {
   const [library, setlibrary] = useState();
   async function handleshowdata(e) {
     console.log(e);
-    // if (category.current.value === "Motibhaiamin1") {
-    //   try {
-    //     const response = await axios.get(
-    //       `https://gram-back.vercel.app/Motibhaiamin1/${e}`
-    //     );
+    if (category.current.value === "Motibhaiamin1") {
+      try {
+        const response = await axios.get(
+          `https://gram-back.vercel.app/Motibhaiamin1/${e}`
+        );
 
     //     navigator("/Motibhaiamin1result", { state: { data: response.data } });
     //     // console.log(response.data), setlibrary(response.data);
