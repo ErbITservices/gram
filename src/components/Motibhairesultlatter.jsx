7fd9010973cc846286f5ff8a51214d44784f2245
MIHIRPATEL2715 <mihirpqtel@gmail.com>
2025-01-09 17:49:16 +0530

    mihir

--- src/components/Motibhairesultlatter.jsx
+++ src/components/Motibhairesultlatter.jsx
@@ -91,11 +91,11 @@ function Motibhairesultlatter() {
             <>
               <div className="latter" ref={pdfref}>
                 <h1> ભલામણ</h1>
-                <h3>
+                <h5>
                   શહેર ગ્રંથાલય કક્ષાના સને {year - 1}-{year} ના વર્ષ ના સ્પર્ધક
                   ગ્રંથાલયોના મૂલ્યાંકન બાદ નીચેની વિગતે ગ્રંથાલયોને ક્રમાનુસાર
                   ભલામણ આ પેટા પ્રાદેશિક સમિતિ કરે છે
-                </h3>
+                </h5>
                 <p className="latterlname">
                   (૧) ઉત્તમ ગ્રંથાલય :- {data[0].lname}{" "}
                   <span className="latterlname spacing">
